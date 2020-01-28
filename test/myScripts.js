853be8b50f5d420d4fa4322659c28b8c40357c94
JordanH305 <55292330+JordanH305@users.noreply.github.com>
2020-01-28 14:14:30 -0500

    Update myScripts.js

--- test/myScripts.js
+++ test/myScripts.js
@@ -37,3 +37,26 @@ var end = document.getElementById("end").value;
     document.write('<p>' + i + '</p>');
   }
 }
+
+
+function myGrades(){
+  var g1 = document.getElementById("grade1").value;
+   var g2 = document.getElementById("grade2").value;
+   var g3 = document.getElementById("grade3").value;
+  var g4 = document.getElementById("grade4").value;
+  
+  g1 = Number(g1);
+  g2 = Number(g2);
+  g3 = Number(g3);
+  g4 = Number(g4);
+  
+  console.log(g1);
+  console.log(g2);
+  console.log(g3);
+  console.log(g4);
+  
+  var low = Math.min(g1, g2, g3, g4);
+var avg = ( g1+g2+g3+g4-low)/3;
+avg = Math.round(avg);
+  document.getElementById("lowest").innerHTML = "Your average without your lowest score is " + avg;
+}
